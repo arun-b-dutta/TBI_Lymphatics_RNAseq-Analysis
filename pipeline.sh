e228e851887b31964a33a4a0b4f1d2f0b3803e7b
Arun Brendan Dutta <abd3x@virginia.edu>
2020-01-21 10:57:22 -0500

    Update junk removal line
    Remove space before 'chr11' and removed chromosomes 20, 21, and 22 b/c they are not present on the mouse genome. Also updated htseq-count line to use the 99 version of the .gtf file, not the 98.

--- pipeline.sh
+++ pipeline.sh
@@ -78,8 +78,8 @@ do
     echo $name
     samtools sort $i -o $i
     samtools index $i
-    samtools view -bh $i chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 \
-             chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM > $name.noJunk.bam
+    samtools view -bh $i chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10/
+        chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY chrM > $name.noJunk.bam
 done
 
 #Removing PCR Duplicates with fixmate and markdup
@@ -102,7 +102,7 @@ do
     name=$(echo $i | awk -F".noDups." '{print $1}')
     echo $name
     samtools view -bf 1 $i | htseq-count -r pos -f bam \
-        --stranded=reverse - Mus_musculus.GRCm38.98.corrected.gtf > $name.gene.counts.txt
+        --stranded=reverse - Mus_musculus.GRCm38.99.corrected.gtf > $name.gene.counts.txt
 done
 
 sh qc.sh
